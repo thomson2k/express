9edcac4a27acd86089c2fabc76c7a67d7e05c020
wirycapybara <w.t.tomasz@gmail.com>
2018-06-17 20:11:21 +0200

    big update

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express')
-
 const server = express()
 
-server.get('/', function (request,response) {
-	response.send('ok')
-})
+
+server.use(express.static('html'))
+//server.get('/', function (request,response) {
+//	console.log('request form',request.connection.remoteAdress,)
+//		
+//	fs.readFile('index.html',funt)
+//
+//	response.send(`
+//		<html>
+//			<head>
+//				<title> Page </title>
+//			
+//			</head>
+//
+//			<body>
+//			<style>	
+//				body {
+//					color:pink;
+//					font-size: 4vh;
+//				}
+//			</style>
+//				helo
+//			</body>
+//		</html>
+//			`)
+//})
 
 server.listen(3000)
